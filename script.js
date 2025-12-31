4a2c1a80e1bb2486a2b9984447790d1a919d7166
4868hm7srm-commits <4868hm7srm@privaterelay.appleid.com>
2025-12-31 11:12:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// PASSWORD
+function cekPassword() {
+    const password = document.getElementById("password").value;
+    if (password === "1234") { // GANTI PASSWORD DI SINI
+        window.location.href = "halo.html";
+    } else {
+        alert("Password salah 😢");
+    }
+}
+
+// MUSIK
+const musik = new Audio("assets/musik.mp3");
+musik.loop = true;
+
+function mulaiMusik() {
+    musik.play();
+}
+
+// EFEK MENGETIK
+function efekKetik(teks, elemenId, kecepatan = 60) {
+    let i = 0;
+    const elemen = document.getElementById(elemenId);
+    elemen.innerHTML = "";
+
+    function ketik() {
+        if (i < teks.length) {
+            elemen.innerHTML += teks.charAt(i);
+            i++;
+            setTimeout(ketik, kecepatan);
+        }
+    }
+    ketik();
+}
